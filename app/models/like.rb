0fd8359c7532965a7fd86c1e6fdf9463a5957dfa
Vanessa Chen <vanchen07@gmail.com>
2021-02-19 08:01:51 -0800

    Add seeds file, add skeleton to chirp, like, and user for w5d5 active record demo

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :bigint           not null, primary key
+#  chirp_id   :integer          not null
+#  liker_id   :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Like < ApplicationRecord
   # Sets up validation for the combination of these two columns
   validates :chirp_id, uniqueness: { scope: :liker_id }
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,25 @@ class User < ApplicationRecord
   has_many :liked_chirps,
     through: :likes,
     source: :chirp
+
+  #Get first user record, use first
+
+  #Get last user record, use last
+
+  #Find a user that exists by id, use find
+
+  #Find a user that doesn't exist by id, use find
+
+  #Find a user by username, use find_by
+  
+  #Find a user by username that does not exist, use find_by
+
+  #Find all users between the ages of 10 and 20 inclusive. Show their username, and political affiliation.
+
+  #Find all users not younger than the age of 11. Use `where.not`
+
+  #Find all political_affiliations of our users
+
+  #Find all users who has a political affiliation in this list and order by ascending
+  #political_affiliations = ["Ruby", "C"]
 end
